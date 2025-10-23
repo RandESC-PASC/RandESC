f83bc348e3b14555bc08ccb1e747c3ba474e75f3
Moritz Gubler <moritz.gubler@e.email>
2025-10-23 12:25:46 +0200

    no linebreak allowed in export statement

--- src/RandESC.jl
+++ src/RandESC.jl
@@ -1,8 +1,7 @@
 module RandESC
 
     # list all functions that should be accessible from outside
-    export rand_ira, ira, lobpcg, sketched_to_fully, randESCSolver, \
-     lobpcg_softlock, lobpcg_lockorder
+    export rand_ira, ira, lobpcg, sketched_to_fully, randESCSolver, lobpcg_softlock, lobpcg_lockorder
 
     # include all source files from this module
     include("sketch.jl")
