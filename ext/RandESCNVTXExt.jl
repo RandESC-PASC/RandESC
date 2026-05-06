693f354404f6db25326cbb5ebac3f6980fc6e797
Augustin Bussy <augustin.bussy@cscs.ch>
2026-05-06 11:59:55 +0200

    More accurate NVTX profiling

--- ext/RandESCNVTXExt.jl
+++ ext/RandESCNVTXExt.jl
@@ -1,10 +1,15 @@
 module RandESCNVTXExt
 using NVTX
 using RandESC
+using CUDA
 
-function __init__()
-	RandESC.timing_start_hook[] = label -> NVTX.range_start(; message=label, color=hash(label) % UInt32)
-	RandESC.timing_end_hook[] = NVTX.range_end
+function RandESC.sync_nvtx_profiling(activate::Bool)
+	if activate
+	    @warn("Accurate NVTX profiling adds GPU synchronization overhead. "*
+	          "Only use 'activate_nvtx_profiling()' if you are actively profiling.")
+	    RandESC.timing_start_hook[] = label -> (CUDA.synchronize(); NVTX.range_start(; message=label, color=hash(label) % UInt32))
+	    RandESC.timing_end_hook[] = token -> (CUDA.synchronize(); NVTX.range_end(token))
+	end
 end
 
 end # module
